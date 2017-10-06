7c3c0b657c5511614176f75173c7de974bb14152
Ashley <ashley.beeson@qa.com>
2017-10-06 14:15:44 +0100

    Update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+function increment(){
+    var num = Number(document.getElementById("baseNum").value);
+    var inc = Number(document.getElementById("addNum").value);
+    window.alert(num + inc);
+}
